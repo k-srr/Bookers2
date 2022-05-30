f266a2effa2689cd4047035db3eab00d3bf234f7
k-srr <sa.12kk28@gmail.com>
2022-05-30 16:26:45 +0000

    [add]layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,13 +16,41 @@ class BooksController < ApplicationController
   end
 
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
     private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body,:profile_image)
   end
 end
